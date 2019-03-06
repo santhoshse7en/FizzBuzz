475e9907a2552fa701712e0cc7765b0021646a7a
M. Santhosh Kumar <47944792+santhoshse7en@users.noreply.github.com>
2019-03-06 19:35:11 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,34 @@
-# Python program to print Fizz Buzz, loop for 50 times i.e. range 
-for fizzbuzz in range(50):
-    
-    # number divisible by 15 (divisible by both 3 & 5), print 'FizzBuzz' in place of the number
-    if fizzbuzz % 3 == 0 and fizzbuzz % 5 == 0:
-        print('FizzBuzz')
-        continue
-        
-    # number divisible by 3, print 'Fizz' in place of the number 
-    elif fizzbuzz % 3 == 0:
-        print('Fizz')
-        continue
-        
-    # number divisible by 5, print 'Buzz' in place of the number   
-    elif fizzbuzz % 5 == 0:
-        print('Buzz')
-        continue
-        
-    # print numbers
-    print(fizzbuzz)
+# Import Packages
+import argparse
+import sys
+
+# Prints Fizz, Buzz, FizzBuzz divisible by 3 and 5 and both
+def fizzbuzzRange(argv):
+    parser = argparse.ArgumentParser(add_help=False, description=('Download reviews from BookMyShow'))
+    parser.add_argument('--help', '-h', action='help', default=argparse.SUPPRESS, help='Show this help message and exit')
+    parser.add_argument('--range', '-r', help='Enter range to print the Fizz, Buzz, FizzBuzz')
+    args = parser.parse_args(argv)
+
+    # Python program to print Fizz Buzz, loop for 50 times i.e. range
+    for fizzbuzz in range(int(args.range)):
+
+        # number divisible by 15 (divisible by both 3 & 5), print 'FizzBuzz' in place of the number
+        if fizzbuzz % 3 == 0 and fizzbuzz % 5 == 0:
+            print('FizzBuzz')
+            continue
+
+        # number divisible by 3, print 'Fizz' in place of the number
+        elif fizzbuzz % 3 == 0:
+            print('Fizz')
+            continue
+
+        # number divisible by 5, print 'Buzz' in place of the number
+        elif fizzbuzz % 5 == 0:
+            print('Buzz')
+            continue
+
+        # print numbers
+        print(fizzbuzz)
+
+if __name__ == '__main__':
+    identifer = fizzbuzzRange(sys.argv[1:])
